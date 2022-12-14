5ac9c61a381f4a1162d2d0793c3a6b4e69e05c28
symit1729 <symit1729@outlook.com>
2022-12-14 19:40:38 +0600

    model and view created in App_Shop

--- App_Shop/urls.py
+++ App_Shop/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
+from App_Shop import views
 app_name = 'App_Shop'
 
 urlpatterns = [
-    
+    path('', views.Home.as_view(), name='home')
 ]
--- App_Shop/views.py
+++ App_Shop/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+# Import Views
+from django.views.generic import ListView, DetailView
+
+#models
+from App_Shop.models import Product
+
 # Create your views here.
+
+class Home(ListView):
+    model = Product
+    template_name = 'App_Shop/home.html'
