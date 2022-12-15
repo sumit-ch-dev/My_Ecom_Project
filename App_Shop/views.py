addb8caf7f06da57a878154c4de0f53d3b21baee
symit1729 <symit1729@outlook.com>
2022-12-15 13:25:43 +0600

    product detail view and template added

--- App_Shop/views.py
+++ App_Shop/views.py
@@ -6,8 +6,15 @@ from django.views.generic import ListView, DetailView
 #models
 from App_Shop.models import Product
 
+#mixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
 class Home(ListView):
     model = Product
     template_name = 'App_Shop/home.html'
+
+
+class ProductDetail(LoginRequiredMixin, DetailView):
+    model = Product
+    template_name = 'App_Shop/product_detail.html'
