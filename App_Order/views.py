fbd7290208e92dc9a6cf1ed74ac279642e3e0c6a
symit1729 <symit1729@outlook.com>
2022-12-16 00:39:09 +0600

    product detail updated

--- App_Order/views.py
+++ App_Order/views.py
@@ -16,21 +16,12 @@ from django.contrib import messages
 @login_required
 def add_to_cart(request, pk):
     item = get_object_or_404(Product, pk=pk)
-    print("Item")
-    print(item)
     order_item = Cart.objects.get_or_create(item=item, user=request.user,
     purchased=False)
-    print("order item object:")
-    print(order_item)
-    print(order_item[0])
     order_qs = Order.objects.filter(user=request.user, ordered=False)
     print("order Qs")
-    print(order_qs)
-    #print(order_qs[0])
     if order_qs.exists():
-        print("if order exists")
         order = order_qs[0]
-        print(order)
         if order.orderitems.filter(item=item).exists():
             order_item[0].quantity += 1
             order_item[0].save()
