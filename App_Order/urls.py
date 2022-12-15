9226ba3224979c8ef081661448e6b5174db67887
symit1729 <symit1729@outlook.com>
2022-12-16 00:49:03 +0600

    cart template added cart_view created

--- App_Order/urls.py
+++ App_Order/urls.py
@@ -5,5 +5,6 @@ from App_Order import views
 app_name = 'App_Order'
 
 urlpatterns = [
-    path('add/<pk>/', views.add_to_cart, name="add")    
+    path('add/<pk>/', views.add_to_cart, name="add"),
+    path('cart/', views.cart_view, name="cart"), 
 ]
