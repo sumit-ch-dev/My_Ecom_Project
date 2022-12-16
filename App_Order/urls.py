f12c65280d0f16029e30c5ecee7c0b00d9962010
symit1729 <symit1729@outlook.com>
2022-12-16 14:34:57 +0600

    modified:   App_Order/urls.py 	modified:   App_Order/views.py 	modified:   templates/App_Order/cart.html

--- App_Order/urls.py
+++ App_Order/urls.py
@@ -6,5 +6,6 @@ app_name = 'App_Order'
 
 urlpatterns = [
     path('add/<pk>/', views.add_to_cart, name="add"),
+    path('remove/<pk>/', views.remove_from_cart, name="remove"),
     path('cart/', views.cart_view, name="cart"), 
 ]
